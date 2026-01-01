2d6f09f40f04679dd39e91efe08e44aaea47d9d3
gnatykdm <gnatykwork@gmail.com>
2026-01-01 20:35:14 +0100

    cblock::CBlock init

--- blockhain/cblock/CBlock.cpp
+++ blockhain/cblock/CBlock.cpp
@@ -2,5 +2,10 @@
 
 namespace blockchain::cblock
 {
-
+	CBlock::CBlock(i64 index, const std::string& previousHash)
+		: bIndex(index), bPreviousHash(previousHash) {
+		bTimestamp = std::time(nullptr);
+		nNonce = 0;
+		calculateHash();
+	}
 } // namespace blockchain::cblock
--- blockhain/cblock/CBlock.h
+++ blockhain/cblock/CBlock.h
@@ -5,10 +5,34 @@
 #include <ctime>
 #include <vector>
 
-#include "../../include/Types.h"
+#include "Types.h"
+#include "CTransaction.h"
 
 namespace blockchain::cblock {
-	class Block {
+	class CBlock {
+	private:
+		const i32 bIndex;										// Position of the block in the blockchain
+		const std::string bPreviousHash;						// Hash of the previous block
+		const std::string bCurrentHash;							// Hash of the current block
+		const std::time_t bTimestamp;							// Time when the block was created
+
+		std::vector<ctransaction::CTransaction> bTransactions;	// List of transactions in the block
+		i32 nNonce;											    // Nonce used for mining
+
+		void calculateHash();									// Method to calculate the block's hash
+	public:
+		explicit CBlock(i64 index, const std::string& previousHash, const std::string& currentHash, std::time_t timestamp);
+
+		void blockMining(i32 difficulty);
+		void addTransaction(const ctransaction::CTransaction& transaction);
+
+		std::string getBlockInfo() const;
+
+		i32 getNonce() const { return nNonde; };
+		i32 getIndex() const { return bIndex; };
+
+		i32 getTransactionCount() const { return bTransactions.size(); };
+		std::time_t getTimestamp() const { return bTimestamp; };
 	};
 }
 
